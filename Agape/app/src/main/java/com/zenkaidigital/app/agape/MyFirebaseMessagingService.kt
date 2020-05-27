2db909e73f0d3a45d6cb9c9b7ab53e363d9d99cc
JJ <jj.orozcolopez.13@gmail.com>
2020-05-26 23:53:44 -0600

    Logo Miest y sobrecarga de método al recibir mensaje.

--- Agape/app/src/main/java/com/zenkaidigital/app/agape/MyFirebaseMessagingService.kt
+++ Agape/app/src/main/java/com/zenkaidigital/app/agape/MyFirebaseMessagingService.kt
@@ -2,9 +2,25 @@ package com.zenkaidigital.app.agape
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+
     override fun onNewToken(token: String) {
-        Log.d("FCM", "Token> " + token)
+        Log.d("FCM", "Refreshed Token: $token")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d("FCM", "From: ${remoteMessage.from}")
+
+        // Check if message contains a data payload.
+        remoteMessage.data.isNotEmpty().let {
+            Log.d("FCM", "Message data payload: " + remoteMessage.data)
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d("FCM", "Message Notification Body: ${it.body}")
+        }
     }
 }
