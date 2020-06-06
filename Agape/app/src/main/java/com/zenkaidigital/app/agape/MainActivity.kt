da97621f4023d73529bf9d12b930df2ffdcd216c
JJ <jj.orozcolopez.13@gmail.com>
2020-06-05 23:58:49 -0600

    Tema de aplicación, colores de icono Agape y diseño de splashscreen.

--- Agape/app/src/main/java/com/zenkaidigital/app/agape/MainActivity.kt
+++ Agape/app/src/main/java/com/zenkaidigital/app/agape/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.zenkaidigital.app.agape.servicio.RestApi
+import com.zenkaidigital.app.agape.servicio.*
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -14,6 +14,11 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Thread.sleep(3000)
+
+        setTheme(R.style.AppTheme)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
